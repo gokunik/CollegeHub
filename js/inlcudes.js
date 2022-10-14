fbe8f166bbe68c54d297dc57d0e4c4bb3df502f3
GokuNik <57342034+gokunik@users.noreply.github.com>
2022-10-14 10:44:45 +0530

    updated navbar

--- js/inlcudes.js
+++ js/inlcudes.js
@@ -21,12 +21,9 @@ if (window.location.pathname.split("/").pop() === "index.html") {
 }
 
 // Remove search bar and menu items form the navbar if the current page is index.html
-let body = document.querySelector("body");
-body.onload = function () {
-  if (window.location.pathname.split("/").pop() === "index.html") {
+ if (window.location.pathname.split("/").pop() === "index.html") {
     let nav = document.querySelector(".nav-wrapper");
     nav.children[1].remove();
     nav.children[1].remove();
     nav.style.justifyContent = "center";
   }
-};
